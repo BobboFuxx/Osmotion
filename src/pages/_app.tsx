e5d6d5c534400f152eaef2b41dc4aa013d3e5314
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-10-02 17:14:48 -0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RewardsProjectionProvider } from "../hooks/useRewardsProjection";
-import { WalletProvider } from "../hooks/useWallet"; // assuming you have a WalletProvider
+import { WalletProvider } from "../hooks/useWallet"; // Provides wallet context
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    // WalletProvider should wrap everything to provide wallet/account info
     <WalletProvider>
+      {/* RewardsProjectionProvider allows modals and tracker to sync projected rewards */}
       <RewardsProjectionProvider>
         <Component {...pageProps} />
       </RewardsProjectionProvider>
